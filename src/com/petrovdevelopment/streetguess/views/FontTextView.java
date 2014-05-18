06272c6d779d67a3a3ac45f479560f2cebf38bd6
Andrey Petrov <rokanor@gmail.com>
2014-05-17 21:12:18 -0400

    added new FontTextView view which can get specific font location in its style. Stylized the buttons

---
+++ src/com/petrovdevelopment/streetguess/views/FontTextView.java
@@ -0,0 +1,45 @@
+package com.petrovdevelopment.streetguess.views;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import com.petrovdevelopment.streetguess.R;
+import com.petrovdevelopment.streetguess.util.U;
+import com.petrovdevelopment.streetguess.util.ViewAttrsUtil;
+
+/**
+ * A text view which uses the special icon flight network font located at assets/fonts/fn.ttf
+ * 
+ * @author Andrey
+ * 
+ */
+public class FontTextView extends TextView {
+	// public static final String ASSETS_FONT_LOCATION = "fonts/flightnetwork.ttf";
+
+	private String mAssetFontLocation;
+
+	public FontTextView(Context context) {
+		super(context);
+		initialize(context);
+	}
+
+	public FontTextView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		ViewAttrsUtil viewAttrsUtil = new ViewAttrsUtil(context, attrs, R.styleable.FontTextView);
+		mAssetFontLocation = viewAttrsUtil.getStringFromXml(R.styleable.FontTextView_font);
+		U.log(this, "font location: " + mAssetFontLocation);
+		if (mAssetFontLocation != null && !mAssetFontLocation.equals("")) {
+			setTypeface(Typeface.createFromAsset(context.getAssets(), mAssetFontLocation));
+		}
+		initialize(context);
+	}
+
+	private void initialize(Context context) {
+		if (!isInEditMode()) {
+
+		}
+	}
+
+}
